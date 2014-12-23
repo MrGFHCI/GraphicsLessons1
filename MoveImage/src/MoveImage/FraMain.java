2a2a02efdef6c310ed74eb48264edf25191dba2a
MrGFHCI <scott.grondin@gmail.com>
2014-12-23 15:42:11 -0500

    Card Layout navigation

---
+++ MoveImage/src/MoveImage/FraMain.java
@@ -0,0 +1,17 @@
+package MoveImage;
+
+import javax.swing.JFrame;
+
+public class FraMain extends JFrame {
+
+    public FraMain() {
+        add(new PanBoard());
+        setTitle("Welp");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200, 400); //1370, 730 for fullscreen for laptop
+        this.setResizable(true);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
---
+++ MoveImage/src/MoveImage/Main.java
@@ -0,0 +1,11 @@
+package MoveImage;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //Stats stats = new Stats();
+        FraMain frame = new FraMain();
+    }
+}
