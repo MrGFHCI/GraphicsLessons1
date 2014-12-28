bc258fa2ec0d3a4fd3f4e4b2360e0cca53c46063
MrGFHCI <scott.grondin@gmail.com>
2014-12-28 14:23:45 -0500

    Card Layout navigation

--- SideScroll2/src/SideScroll2/FraRunner.java
+++ SideScroll2/src/SideScroll2/FraRunner.java
@@ -2,7 +2,6 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-From http://www.nutt.net/2013/11/25/create-scrolling-background-java/
  */
 package SideScroll2;
 import java.awt.Component;
