4acc394df8823f0087cea9217a1906a9a1b83952
grondir <grondir@FHC-IC0026784.wrdsb.ca>
2015-01-06 08:44:35 -0500

    Cleaned up the sprite sheet animation

--- SpriteSheetAnim/src/SpriteSheetAnim/Main.java
+++ SpriteSheetAnim/src/SpriteSheetAnim/Main.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        //Stats stats = new Stats();
         FraMain frame = new FraMain();
     }
 }
